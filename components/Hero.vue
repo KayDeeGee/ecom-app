8325ff53bfd803a67fc964451441d7b0ae5d5bdf
KayDeeGee <kylebryanbasco@gmail.com>
2024-09-20 16:01:26 +0800

    init commit, added hero, navbar components. added css w/tailwind configuration

---
+++ components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="grid grid-cols-11 gap-4 min-h-[716px] w-full">
+        <div class="col-span-1 bg-slate-300">putangina</div>
+        <div class="col-span-9 grid grid-cols-2 gap-1 bg-slate-300">
+            <div class=" bg-slate-400">
+                asdawsd
+            </div>
+            <div class="grid grid-rows-2 gap-1 bg-slate-400">
+                <div class="row-span-1 bg-slate-500">wawa</div>
+                <div class="row-span-1 bg-slate-500">
+                    <div class="grid grid-cols-12 gap-1 flex-1 h-full">
+                        <div class="grid grid-rows-2 gap-1 col-span-6 bg-slate-200">
+                            <div class="row-span-1 bg-white">asdasd</div>
+                            <div class="row-span-1 bg-white">asdasd</div>
+                        </div>
+                        <div class="col-span-6 bg-slate-200">asdasd</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-1 bg-slate-300">putangina</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Hero />
+    </div>
+</template>
+
+<script setup>
+const { data, status, error, refresh, clear } = await useFetch('https://api.escuelajs.co/api/v1/products')
+</script>
+
+<style scoped></style>
